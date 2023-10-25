b90caa3ad3b24398e808ce94b1704ee3f2ac2ff6
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-10-25 16:55:31 +0530

    appwrite config integration

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -11,36 +11,26 @@ export class AuthService {
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
-            
     }
 
     async createAccount({email, password, name}) {
-        try {
-            const userAccount = await this.account.create(ID.unique(), email, password, name);
-            if (userAccount) {
-                // call another method
-                return this.login({email, password});
-            } else {
-               return  userAccount;
-            }
-        } catch (error) {
-            throw error;
+        const userAccount = await this.account.create(ID.unique(), email, password, name);
+        if (userAccount) {
+            return this.login({email, password});
+        } else {
+            return userAccount;
         }
     }
 
     async login({email, password}) {
-        try {
-            return await this.account.createEmailSession(email, password);
-        } catch (error) {
-            throw error;
-        }
+        return await this.account.createEmailSession(email, password);
     }
 
     async getCurrentUser() {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite serive :: getCurrentUser :: error", error);
+            console.log("Appwrite service :: getCurrentUser :: error", error);
         }
 
         return null;
@@ -51,7 +41,7 @@ export class AuthService {
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite serive :: logout :: error", error);
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 }
